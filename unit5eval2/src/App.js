f0bf7e6c028b2e8653d9fa1757c0104169ffa1fb
pavan <pavan_fw10_057@masai.school>
2021-11-08 16:39:42 +0530

    new commit

--- unit5eval2/src/App.js
+++ unit5eval2/src/App.js
@@ -1,11 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import HomePage from './Components/HomePage/HomePage';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        
+      <HomePage/>
+      
     </div>
   );
 }
--- unit5eval2/src/Components/HomePage/HomePage.jsx
+++ unit5eval2/src/Components/HomePage/HomePage.jsx
@@ -1,8 +1,40 @@
-import React from 'react'
+import React from 'react';
+import axios from 'axios';
+import { Card, CardContent, Typography } from '@material-ui/core';
+
 
 function HomePage() {
+    const [list,setList] = React.useState([]);
+    React.useEffect(()=>{
+        getData()
+    },[])
+
+    async function getData(){
+        const {data} = await axios.get('https://rickandmortyapi.com/api/character')
+        setList(data.results)
+    }
     return (
         <div>
+            <div>
+                {list.map((item)=>{
+                    return (
+                    <Card style={{width:300,backgroundColor:'red'}}>
+                        <CardContent>
+                            <Typography style={{fontSize:15}}>
+                                {item.name}
+                            </Typography>
+                            <Typography style={{fontSize:15}}>
+                                {item.status}
+                            </Typography>
+                            <Typography style={{fontSize:15}}>
+                                {item.species}
+                            </Typography>
+                            <img src={item.image} alt="/"/>
+                        </CardContent>
+                    </Card>
+                    )
+                    })}
+            </div>
             
         </div>
     )
